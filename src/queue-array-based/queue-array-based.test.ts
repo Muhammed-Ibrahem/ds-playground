7e032112776e534c6152fc8a50209b20b2553f76
Muhammed Ibrahem <muhammedibrahem001@gmail.com>
2025-06-25 03:12:19 +0300

    feat: implement array-based circular queue with dynamic resizing

---
+++ src/queue-array-based/queue-array-based.test.ts
@@ -0,0 +1,64 @@
+import { Queue } from "./queue-array-based";
+
+describe("Queue Array Based", () => {
+  describe("enqueue", () => {
+    it("should push new data to the queue", () => {
+      const q = new Queue<number>();
+
+      q.enqueue(1);
+
+      expect(q.size).toBe(1);
+      expect(q.peek()).toBe(1);
+    });
+    it("shoud resize if reaching the end of the capacity", () => {
+      const q = new Queue<number>(5, 1, 2, 3, 4, 5);
+      q.enqueue(6);
+      expect(q.size).toBe(6);
+    });
+  });
+
+  describe("dequeue", () => {
+    it("should return null if the queue is empty", () => {
+      const q = new Queue<number>();
+
+      const data = q.dequeue();
+
+      expect(data).toBeNull();
+      expect(q.size).toBe(0);
+      expect(q.peek()).toBeNull();
+    });
+    it("should remove data from the queue and return its value", () => {
+      const q = new Queue<number>(16, 1, 2, 3);
+
+      const data = q.dequeue();
+
+      expect(data).toBe(1);
+      expect(q.size).toBe(2);
+      expect(q.peek()).toBe(2);
+    });
+  });
+
+  describe("peek", () => {
+    it("shoud return the value of the first item of the queue without removing it", () => {
+      const q = new Queue<number>(16, 1, 2, 3);
+
+      const data = q.peek();
+
+      expect(data).toBe(1);
+      expect(q.size).toBe(3);
+    });
+  });
+
+  describe("hasData", () => {
+    it("should return false if queue is empty", () => {
+      const q = new Queue<number>();
+
+      expect(q.hasData()).toBe(false);
+    });
+    it("should return true if queue has atleast one value", () => {
+      const q = new Queue<number>(16, 1);
+
+      expect(q.hasData()).toBe(true);
+    });
+  });
+});
